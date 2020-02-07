86bca97af1d8de5e86d384f65b3b9e91cec4d8fe
abl3l <7ckngMad.mn@gmail.com>
2020-02-07 14:34:51 +0200

    Updates

---
+++ precache-manifest.58e914283e473a7c6321bbce8871acb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07ce8e8d4e44ece41e1af1d9f98965d7",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "883ecd4f8ee8b515ca14",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.a6a6c902.chunk.css"
+  },
+  {
+    "revision": "d09f0249c7eef31cba78",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.6cc67902.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.6cc67902.chunk.js.LICENSE"
+  },
+  {
+    "revision": "883ecd4f8ee8b515ca14",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.c1287056.chunk.js"
+  },
+  {
+    "revision": "8b583280b34de9d01efa",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.8935b325.js"
+  }
+]);
